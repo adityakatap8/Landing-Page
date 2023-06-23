a69c5079ed50be02e4349a8e04960709f5d571ae
UpcloudTechnologies <78796329+adityakatap8@users.noreply.github.com>
2023-06-23 14:58:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,9 @@
-// document.addEventListener("DOMContentLoaded", function() {
-   
-//     const carousel = document.querySelector(".carousel");
-//     const slides = Array.from(document.querySelectorAll(".carousel-slide"));
-//     const leftArrow = document.querySelector(".carousel-arrow-left");
-//     const rightArrow = document.querySelector(".carousel-arrow-right");
-//     let currentSlideIndex = 0;
-  
-//     // Display the initial slide
-//     slides[currentSlideIndex].classList.add("active");
-
-  
-//     // Function to show the next slide
-//     function showNextSlide() {
-//       slides[currentSlideIndex].classList.remove("active");
-//       currentSlideIndex = (currentSlideIndex + 1) % slides.length;
-//       slides[currentSlideIndex].classList.add("active");
-//     }
-  
-//     // Function to show the previous slide
-//     function showPreviousSlide() {
-//       slides[currentSlideIndex].classList.remove("active");
-//       currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
-//       slides[currentSlideIndex].classList.add("active");
-//     }
-  
-//     // Function to handle left arrow click
-//     function handleLeftArrowClick() {
-//       showPreviousSlide();
-//     }
-  
-//     // Function to handle right arrow click
-//     function handleRightArrowClick() {
-//       showNextSlide();
-//     }
-  
-//     // Add event listeners to the arrow buttons
-//     leftArrow.addEventListener("click", handleLeftArrowClick);
-//     rightArrow.addEventListener("click", handleRightArrowClick);
-//   });
 
 
 
+function clickContact() {
+  window.location.href = "contact.html";
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     const carousel = document.querySelector(".carousel");
